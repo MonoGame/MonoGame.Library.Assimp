ded0fe963d029456663c574a037deb04986f341a
Dean Ellis <dellis1972@googlemail.com>
2024-02-22 23:20:25 +0000

    build windows using MSBuild

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -11,9 +11,16 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-         var buildWorkingDir = "assimp/";
+        var buildWorkingDir = "assimp/";
+        MSBuildSettings buildSettings = new()
+        {
+            Verbosity = Verbosity.Normal,
+            Configuration = "Release",
+            PlatformTarget = PlatformTarget.x64
+        };
         //  Disable openmp so there is no dependency on VCOMP140.dll
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF CMakeLists.txt"});
+        context.MSBuild("assimp/ALL_BUILD.vcxproj", buildSettings);
         context.StartProcess("dir", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = ""});
         context.CopyFile("assimp/bin/assimp.dll", $"{context.ArtifactsDir}/assimp.dll");
     }

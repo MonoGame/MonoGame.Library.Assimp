4c8d6e0a27d9f6d7281c5767f5208597f821e1b5
Dean Ellis <dellis1972@googlemail.com>
2024-02-22 22:40:48 +0000

    try just x86_64 and arm64

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -12,7 +12,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "assimp/";
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DCMAKE_OSX_ARCHITECTURES=\"x86_64 x86 arm64\" CMakeLists.txt"});
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt"});
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = ""});
         context.CopyFile(@"assimp/bin/libassimp.5.3.0.dylib", $"{context.ArtifactsDir}/libassimp.dylib");
     }

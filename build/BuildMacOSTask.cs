b40b0d10d75bf39a1a76c8430bc328d81d3a81d4
Dean Ellis <dellis1972@googlemail.com>
2024-02-22 23:42:12 +0000

    Remove version for MacOS file

--- build/BuildMacOSTask.cs
+++ build/BuildMacOSTask.cs
@@ -14,6 +14,7 @@ public sealed class BuildMacOSTask : FrostingTask<BuildContext>
         var buildWorkingDir = "assimp/";
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DCMAKE_OSX_ARCHITECTURES=\"x86_64;arm64\" CMakeLists.txt"});
         context.StartProcess("make", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = ""});
-        context.CopyFile(@"assimp/bin/libassimp.5.3.0.dylib", $"{context.ArtifactsDir}/libassimp.dylib");
+        var files = System.IO.Directory.GetFiles (System.IO.Path.Combine (buildWorkingDir, "bin"), "libassimp.*.*.*.dylib", SearchOption.TopDirectoryOnly);
+        context.CopyFile(files[0], $"{context.ArtifactsDir}/libassimp.dylib");
     }
 }

ee8a4b349beeb2034225fd67906e7719f2da22e8
Dean Ellis <dellis1972@googlemail.com>
2024-02-23 12:55:57 +0000

    actually fix up the vcxproj files

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -12,8 +12,9 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
     public override void Run(BuildContext context)
     {
         var buildWorkingDir = "assimp/";
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DMSVC_RUNTIME_LIBRARY=MultiThreaded CMakeLists.txt"});
-        context.ReplaceTextInFiles("assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF CMakeLists.txt"});
+        context.ReplaceTextInFiles("assimp/code/assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles("assimp/contrib/zlib/zlibstatic.vcxproj", "MultiThreadedDLL", "MultiThreaded");
         context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release"});
         context.CopyFile(@"assimp/bin/Release/assimp-vc143-mt.dll", $"{context.ArtifactsDir}/assimp.dll");
     }

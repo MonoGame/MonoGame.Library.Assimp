46bcc3853550186f0b27270d18ebb86e96879494
Dean Ellis <dellis1972@googlemail.com>
2026-02-07 13:10:51 +0000

    Add support for building ARM64 architecture (#6)
    * Add support for building ARM64 architecture
    
    * add linux arm

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -11,11 +11,29 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
 
     public override void Run(BuildContext context)
     {
-        var buildWorkingDir = "assimp/";
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "-DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF CMakeLists.txt" });
-        context.ReplaceTextInFiles("assimp/code/assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
-        context.ReplaceTextInFiles("assimp/contrib/zlib/zlibstatic.vcxproj", "MultiThreadedDLL", "MultiThreaded");
-        context.StartProcess("cmake", new ProcessSettings { WorkingDirectory = buildWorkingDir, Arguments = "--build . --config release" });
-        context.CopyFile(@"assimp/bin/Release/assimp-vc143-mt.dll", $"{context.ArtifactsDir}/assimp.dll");
+        BuildForArchitecture(context, "x64", "win-x64");
+        BuildForArchitecture(context, "ARM64", "win-arm64");
+    }
+
+    private void BuildForArchitecture(BuildContext context, string cmakeArch, string rid)
+    {
+        var buildWorkingDir = $"assimp/build_{rid}";
+        context.CreateDirectory(buildWorkingDir);
+        
+        context.StartProcess("cmake", new ProcessSettings { 
+            WorkingDirectory = buildWorkingDir, 
+            Arguments = $"-A {cmakeArch} -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF .." 
+        });
+        
+        context.ReplaceTextInFiles($"{buildWorkingDir}/code/assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        context.ReplaceTextInFiles($"{buildWorkingDir}/contrib/zlib/zlibstatic.vcxproj", "MultiThreadedDLL", "MultiThreaded");
+        
+        context.StartProcess("cmake", new ProcessSettings { 
+            WorkingDirectory = buildWorkingDir, 
+            Arguments = "--build . --config release" 
+        });
+        
+        context.CreateDirectory($"{context.ArtifactsDir}/{rid}");
+        context.CopyFile($@"{buildWorkingDir}/bin/Release/assimp-vc143-mt.dll", $"{context.ArtifactsDir}/{rid}/assimp.dll");
     }
 }

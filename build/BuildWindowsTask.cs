267862b5e28925f122e436faaf91a8cb7e26976e
Dean Ellis <dellis1972@googlemail.com>
2026-07-11 20:10:15 +0100

    TRy this

--- build/BuildWindowsTask.cs
+++ build/BuildWindowsTask.cs
@@ -22,7 +22,7 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         
         context.StartProcess("cmake", new ProcessSettings { 
             WorkingDirectory = buildWorkingDir, 
-            Arguments = $"-A {cmakeArch} -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF .." 
+            Arguments = $"-A {cmakeArch} -DASSIMP_BUILD_ZLIB=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF .." 
         });
         
         context.ReplaceTextInFiles($"{buildWorkingDir}/code/assimp.vcxproj", "MultiThreadedDLL", "MultiThreaded");
@@ -34,6 +34,6 @@ public sealed class BuildWindowsTask : FrostingTask<BuildContext>
         });
         
         context.CreateDirectory($"{context.ArtifactsDir}/{rid}");
-        context.CopyFile($@"{buildWorkingDir}/bin/Release/assimp-vc143-mt.dll", $"{context.ArtifactsDir}/{rid}/assimp.dll");
+        context.CopyFile($@"{buildWorkingDir}/bin/Release/assimp-vc145-mt.dll", $"{context.ArtifactsDir}/{rid}/assimp.dll");
     }
 }
